2d7cb8c9263e5e7399638ec9380a8b78e7d3368f
duythinh26 <pduythinh79@gmail.com>
2024-04-21 21:06:36 +0700

    change the structure of the trending novel part

--- client/src/components/pages/Homepage.jsx
+++ client/src/components/pages/Homepage.jsx
@@ -1,11 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import axios from "axios";
 import TrendingNovel from '../common/TrendingNovel';
+import Discussion from '../common/Discussion';
 
 const Homepage = () => {
     return (
         <main className="w-full pt-nav pb-0">
-            <TrendingNovel />
+            <div className="container w-full mx-auto px-[15px] mb-[40px]">
+                <div className="flex flex-wrap mx-[-15px]">
+                    <TrendingNovel />
+                    <Discussion />
+                </div>
+            </div>
+            <div className="text-center mt-0 mb-[10px] mx-auto"></div>
         </main>
     )
 }
